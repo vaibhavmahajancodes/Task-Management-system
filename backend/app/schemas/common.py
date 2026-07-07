dc1d60ca93e00b4f1be64449cb67f84b3fa89f01
Vaibhav Mahajan <mahajanvaibhav5001@gmail.com>
2026-07-07 12:50:48 +0530

    Update common.py

--- backend/app/schemas/common.py
+++ backend/app/schemas/common.py
@@ -1,17 +1,17 @@
 from typing import Generic, List, TypeVar
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 T = TypeVar("T")
 
 
 class Page(BaseModel, Generic[T]):
     items: List[T]
-    total: int
-    page: int
-    page_size: int
-    pages: int
+    total: int = Field(ge=0)
+    page: int = Field(ge=1)
+    page_size: int= Field(ge=1)
+    pages: int = Field(ge=0)
 
 
 class Message(BaseModel):
-    message: str
+    message: str = Field(min_length=1, max_lengh=500)
